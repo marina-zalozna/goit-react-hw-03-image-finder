3b23b2d28bba3dac02729dd768091f1f52faaa1f
Marina Zalozna <marina.zalozna@gmail.com>
2023-01-13 13:21:17 +0100

    Correct HW

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -4,22 +4,27 @@ import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import css from './ImageGallery.module.css';
 
 export function ImageGallery({ cards, onOpen }) {
-    const Elements = cards.map(({ id, webformatURL, largeImageURL }) => {
-        
-        return (<ImageGalleryItem key={id} webformatURL={webformatURL} largeImageURL={largeImageURL} onOpen={onOpen} />)
-    })
-    return (
-        <ul className={css.ImageGallery}>
-        {Elements && Elements}  
-        </ul>
-    )
+  return (
+    <ul className={css.ImageGallery}>
+      {this.props.cards.map(({ id, webformatURL, largeImageURL }) => {
+        <ImageGalleryItem
+          key={id}
+          webformatURL={webformatURL}
+          largeImageURL={largeImageURL}
+          onOpen={onOpen}
+        />;
+      })}
+    </ul>
+  );
 }
 
 ImageGallery.propTypes = {
-    onOpen: PropTypes.func.isRequired,
-    cards: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        webformatURL: PropTypes.string.isRequired,
-        largeImageURL: PropTypes.string.isRequired
-    }))
-}
+  onOpen: PropTypes.func.isRequired,
+  cards: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      webformatURL: PropTypes.string.isRequired,
+      largeImageURL: PropTypes.string.isRequired,
+    })
+  ),
+};
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,47 +1,44 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { createPortal } from 'react-dom';
-import css from './Modal.module.css'
-import PropTypes from "prop-types";
+import css from './Modal.module.css';
+import PropTypes from 'prop-types';
 
 const modalRoot = document.getElementById('root');
 
 export class Modal extends Component {
-        
-    componentDidMount() {
-        window.addEventListener('keydown', this.handleKeyDown);
-        window.addEventListener('click', this.handleCloseClick)
-    }
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
 
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.handleKeyDown);
-        window.removeEventListener('click', this.handleCloseClick)
-    }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
 
-    handleKeyDown = e => {
-        if (e.code === 'Escape') {
-        return this.props.onClose();
-        }
-        };
-        
-        handleCloseClick = e => {
-
-            if (e.target.alt === undefined ) {
-                return this.props.onClose();
-            }      
-        }
-
-    render() {
-        return createPortal(
-        <div className={css.Overlay}>
-            <div className={css.Modal}>
-            <img src={this.props.modalImage} alt="modalPicture" />
-            </div>
-        </div>, modalRoot
-        );
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
     }
+  };
+
+  handleCloseClick = e => {
+    if (e.currenttarget !== e.target) {
+      this.props.onClose();
     }
+  };
+
+  render() {
+    return createPortal(
+      <div className={css.Overlay} onClick={this.handleCloseClick}>
+        <div className={css.Modal}>
+          <img src={this.props.modalImage} alt="modalPicture" />
+        </div>
+      </div>,
+      modalRoot
+    );
+  }
+}
 
 Modal.propTypes = {
-    onClose: PropTypes.func.isRequired,
-    modalImage: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
+  modalImage: PropTypes.string.isRequired,
 };
